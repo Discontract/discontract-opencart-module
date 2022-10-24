8bd833ad467d9b2b40903bb0d74843ba71eb6afa
Tautvilas Mečinskas <connect@tautvilas.lt>
2022-10-24 13:43:54 +0300

    settings read/write

--- admin/controller/extension/module/discontract.php
+++ admin/controller/extension/module/discontract.php
@@ -7,21 +7,18 @@ class ControllerExtensionModuleDiscontract extends Controller {
 
 		$this->document->setTitle($this->language->get('heading_title'));
 
-		$this->load->model('setting/module');
+		$this->load->model('setting/setting');
 
 		// 	$this->cache->delete('product');
 
 		// 	$this->session->data['success'] = $this->language->get('text_success');
 
 		// 	$this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true));
-		// }
+		// 
 		if (($this->request->server['REQUEST_METHOD'] == 'POST')) {
-			$this->request->post['name'] = 'discontract';
-			if (!isset($this->request->get['module_id'])) {
-				$this->model_setting_module->addModule('discontract', $this->request->post);
-			} else {
-				$this->model_setting_module->editModule($this->request->get['module_id'], $this->request->post);
-			}
+			// var_dump($this->request->post);
+			$this->model_setting_setting->editSetting('module_discontract', $this->request->post);
+			$this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true));
 
 			// $this->cache->delete('product');
 
@@ -30,22 +27,15 @@ class ControllerExtensionModuleDiscontract extends Controller {
 			// $this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true));
 		}
 
-		if (!isset($this->request->get['module_id'])) {
-			$data['action'] = $this->url->link('extension/module/discontract', 'user_token=' . $this->session->data['user_token'], true);
-		} else {
-			$data['action'] = $this->url->link('extension/module/discontract', 'user_token=' . $this->session->data['user_token'] . '&module_id=' . $this->request->get['module_id'], true);
-		}
+		$data['action'] = $this->url->link('extension/module/discontract', 'user_token=' . $this->session->data['user_token'], true);
 		$data['cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true);
 
 		$data['header'] = $this->load->controller('common/header');
 		$data['column_left'] = $this->load->controller('common/column_left');
 		$data['footer'] = $this->load->controller('common/footer');
 
-		$module_info = array();
-		if (isset($this->request->get['module_id'])) {
-			$module_info = $this->model_setting_module->getModule($this->request->get['module_id']);
-			$data['api_key'] = $module_info['api_key'];
-		}
+		$data['module_discontract_status'] = $this->config->get('module_discontract_status');
+		$data['module_discontract_api_key'] = $this->config->get('module_discontract_api_key');
 
 		$this->response->setOutput($this->load->view('extension/module/discontract', $data));
 	}
