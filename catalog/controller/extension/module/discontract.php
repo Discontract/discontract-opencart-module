c22c2890e4ae0274e58768bb5406984452048ff0
Tautvilas Mečinskas <connect@tautvilas.lt>
2022-11-07 14:46:52 +0200

    get places suggestions

--- catalog/controller/extension/module/discontract.php
+++ catalog/controller/extension/module/discontract.php
@@ -15,11 +15,13 @@ class ControllerExtensionModuleDiscontract extends Controller {
     }
 	}
   
-  public function getSuggestions() {
-    $data = array();
-    $data['one'] = 'two';
+  public function locations() {
+    $postCode = $this->request->post['postCode'];
+    $this->load->model('extension/discontract/api');
+    $locations = $this->model_extension_discontract_api->getLocations($postCode);
+    // die('[{"postCode":"11329","lat":"54.651886","lng":"25.348360","description":"Airi\u0173 g. 1, Vilnius"},{"postCode":"11329","lat":"54.652248","lng":"25.348738","description":"Airi\u0173 g. 2, Vilnius"},{"postCode":"11329","lat":"54.651775","lng":"25.348730","description":"Airi\u0173 g. 3, Vilnius"},{"postCode":"11329","lat":"54.652195","lng":"25.349113","description":"Airi\u0173 g. 4, Vilnius"},{"postCode":"11329","lat":"54.651669","lng":"25.349117","description":"Airi\u0173 g. 5, Vilnius"},{"postCode":"11329","lat":"54.652145","lng":"25.349464","description":"Airi\u0173 g. 6, Vilnius"},{"postCode":"11329","lat":"54.652550","lng":"25.348812","description":"Angl\u0173 g. 1, Vilnius"},{"postCode":"11329","lat":"54.653004","lng":"25.348902","description":"Angl\u0173 g. 2, Vilnius"},{"postCode":"11329","lat":"54.652431","lng":"25.349342","description":"Angl\u0173 g. 3, Vilnius"},{"postCode":"11329","lat":"54.653214","lng":"25.349165","description":"Angl\u0173 g. 4, Vilnius"}]');
     $this->response->addHeader('Content-Type: application/json');
-    $this->response->setOutput(json_encode($data));
+    $this->response->setOutput(json_encode($locations));
   }
 
   public function addToCart() {
